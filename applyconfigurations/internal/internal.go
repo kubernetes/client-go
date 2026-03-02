27966b25746df8e0b2fe5ee07042d568d194d0f8
Hemant Kumar <hekumar@redhat.com>
2026-03-02 17:00:43 -0500

    Update generated code
    Kubernetes-commit: 143c56ddeb8ba470102d5145bbfafca898999763

--- applyconfigurations/internal/internal.go
+++ applyconfigurations/internal/internal.go
@@ -15043,9 +15043,6 @@ var schemaYAML = typed.YAMLObject(`types:
 - name: io.k8s.api.storage.v1beta1.CSIDriverSpec
   map:
     fields:
-    - name: PreventPodSchedulingIfMissing
-      type:
-        scalar: boolean
     - name: attachRequired
       type:
         scalar: boolean
@@ -15056,6 +15053,9 @@ var schemaYAML = typed.YAMLObject(`types:
       type:
         scalar: numeric
     - name: podInfoOnMount
+      type:
+        scalar: boolean
+    - name: preventPodSchedulingIfMissing
       type:
         scalar: boolean
     - name: requiresRepublish
